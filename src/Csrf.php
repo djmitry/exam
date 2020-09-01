4f09db888dffb7d8c34a0bf5b91a8796755542d9
Dmitry <dimazagainov1@gmail.com>
2020-09-01 17:36:47 +0600

    add csrf class

---
+++ src/Csrf.php
@@ -0,0 +1,15 @@
+<?php
+
+class Csrf
+{
+    public function createToken(): string
+    {
+        $_SESSION['csrf_token'] = uniqid('app', true);
+        return $_SESSION['csrf_token'];
+    }
+
+    public function validate($token): bool
+    {
+        return isset($_SESSION['csrf_token']) ? $_SESSION['csrf_token'] === $token : false;
+    }
+}
