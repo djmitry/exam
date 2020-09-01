0795d02ee3979b1b9152429efb7b9423951081b9
Dmitry <dimazagainov1@gmail.com>
2020-09-01 17:27:54 +0600

    add feedback class with validate and save

---
+++ src/feedback.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Feedback
+ */
+class Feedback
+{
+    /**
+     * Validate input
+     */
+    public function validate($data)
+    {
+        $name = $data['name'] ?? '';
+        $email = $data['email'] ?? '';
+        $text = $data['text'] ?? '';
+
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+        if (!$name) {
+            $errors[] = "Wrong name";
+        }
+
+        if ( filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+            $errors[] = "Wrong email";
+        }
+
+        $text = filter_var($text, FILTER_SANITIZE_STRING);
+        if (!$text) {
+            $errors[] = "Wrong Text";
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Save 
+     * TODO: Do save 
+     */
+    public function save()
+    {
+        return true;
+    }
+}
