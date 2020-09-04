202abd8097cc72c0fae4abf043d8f93a3f173cfd
Dmitry <dimazagainov1@gmail.com>
2020-09-04 14:54:36 +0600

    add css

--- src/views/form.php
+++ src/views/form.php
@@ -2,25 +2,25 @@
 
 use App\Csrf;
 
-foreach($this->errors as $error) {
 ?>
-    <div class="alert alert-danger"><?= $error ?></div>
-<?php
-}
-?>
-<form action="index.php" method="POST">
-    <input type="hidden" name="form" value="feedback">
-    <input type="hidden" name="csrf_token" value="<?= Csrf::create() ?>">
-    <div>
-        <input type="text" name="name" value="<?= $_POST['name'] ?? '' ?>">
-    </div>
-    <div>
-        <input type="text" name="email" value="<?= $_POST['email'] ?? '' ?>">
-    </div>
-    <div>
-        <textarea name="text" id="" cols="30" rows="10"><?= $_POST['text'] ?? '' ?></textarea>
-    </div>
-    <div>
-        <button type="submit">Send</button>
-    </div>
-</form>
+<div class="form-feedback">
+    <?php foreach($this->errors as $error) { ?>
+        <div class="alert alert-danger"><?= $error ?></div>
+    <?php } ?>
+    <form action="index.php" method="POST">
+        <input type="hidden" name="form" value="feedback">
+        <input type="hidden" name="csrf_token" value="<?= Csrf::create() ?>">
+        <div class="form-group">
+            <input type="text" name="name" value="<?= $_POST['name'] ?? '' ?>">
+        </div>
+        <div class="form-group">
+            <input type="text" name="email" value="<?= $_POST['email'] ?? '' ?>">
+        </div>
+        <div class="form-group">
+            <textarea name="text" id="" rows="10"><?= $_POST['text'] ?? '' ?></textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit">Send</button>
+        </div>
+    </form>
+</div>
--- src/views/layout.php
+++ src/views/layout.php
@@ -7,16 +7,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App</title>
+    <link rel="stylesheet" href="src/public/style.css">
 </head>
 <body>
-    <?php if ($this->flash->has('success')) { ?>
-        <div class="alert alert-success"><?= $this->flash->get('success') ?></div>
-    <?php } ?>
-
-    <?php if ($this->flash->has('error')) { ?>
-        <div class="alert alert-danger"><?= $this->flash->get('error') ?></div>
-    <?php } ?>
-
-    <?= $form->render() ?>
+    <div class="container">
+        <?php if ($this->flash->has('success')) { ?>
+            <div class="alert alert-success"><?= $this->flash->get('success') ?></div>
+        <?php } ?>
+    
+        <?php if ($this->flash->has('error')) { ?>
+            <div class="alert alert-danger"><?= $this->flash->get('error') ?></div>
+        <?php } ?>
+    
+        <?= $form->render() ?>
+    </div>
 </body>
 </html>
